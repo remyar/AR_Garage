382e5c92e60d7d796639684bc977bd3552cbecbd
Rémy <remy.arnaudin@sauermanngroup.com>
2023-02-20 16:18:53 +0100

    del

--- assets/donotremove.bat
+++ assets/donotremove.bat
@@ -4,4 +4,9 @@ wget -O tesseract-ocr-w64-setup-v5.2.0.20220712.exe https://digi.bib.uni-mannhei
 7z x tesseract-ocr-w64-setup-v5.2.0.20220712.exe
 del tesseract-uninstall.exe
 del tesseract-ocr-w64-setup-v5.2.0.20220712.exe
+cd..
+cd database
+wget -O tecdoc_database.zip https://www.goodrace.fr/download/tecdoc_database.zip
+7z x tecdoc_database.zip
+del tecdoc_database.zip
 pause
