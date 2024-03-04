fba0a84e221d0ccf7c400d8be480e3858fc27d4d
Rémy <remy.arnaudin@sauermanngroup.com>
2024-03-04 09:03:14 +0100

    Update all.vehicules.js

--- src/actions/get/all.vehicules.js
+++ src/actions/get/all.vehicules.js
@@ -1,13 +1,29 @@
 import createAction from '../../middleware/actions';
 import actions from '../../actions';
 
-export async function getAllVehicules({ dispatch , extra, getState }) {
+export async function getAllVehicules({ dispatch, extra, getState }) {
     const database = extra.database;
 
     try {
         let vehicules = await database.getAllVehicules();
         for (let vehicule of vehicules) {
             vehicule.hasTechnics = false;
+            if (vehicule.image == undefined) {
+                if (vehicule.tecdocId != undefined) {
+                    try {
+                        let data = await dispatch(actions.technics.getModelByTecdocId(vehicule.tecdocId));
+                        let model = data?.model || {};
+                        if (model.imageId) {
+                            let image = await dispatch(actions.get.images(model.imageId));
+                            vehicule.image = { ...image.image }
+                        }
+                    } catch (err) {
+                    
+                    } finally{
+                        await database.saveVehicule(vehicule);
+                    }
+                }
+            }
         }
         return {
             vehicules: vehicules
